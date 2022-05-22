ba8861130e450d9bd0c433a10ec6521ee109288c
Douglas <douglas.159@hotmail.com>
2022-05-22 14:49:44 -0300

    Main Screen with Team plus Player Button

---
+++ app/src/main/java/br/com/douglas/esports/MainFragment.kt
@@ -0,0 +1,37 @@
+package br.com.douglas.esports
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import br.com.douglas.esports.databinding.FragmentMainBinding
+
+
+class MainFragment : Fragment() {
+
+    private lateinit var binding: FragmentMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentMainBinding.inflate(inflater,container,false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.btnPlayers.setOnClickListener {
+            findNavController().navigate(R.id.goToPlayers)
+        }
+    }
+
+
+}
